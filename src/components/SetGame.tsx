1c5d7d0d7ceadf8b3bc75cfcb35db7b2dd230615
Steve Messing <stevemessing999@gmail.com>
2024-06-04 22:24:49 -0700

    deck state management works!

--- src/components/SetGame.tsx
+++ src/components/SetGame.tsx
@@ -1,61 +1,58 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { LiveCardProps } from "./interfaces";
 import getStartingDeck from "./SetDeck";
 import SetHand from "./SetHand";
 
 export const SetGame: React.FC = () => {
-  var deck = getStartingDeck();
+  var initialDeck = getStartingDeck();
+  const [deck, setDeck] = useState(initialDeck);
+  const [hand, setHand] = useState<LiveCardProps[]>([]);
 
   function drawCards(count: number): LiveCardProps[] {
     const cards = deck.slice(0, count);
-    deck = deck.slice(count);
+    setDeck(deck.slice(count));
+    console.log(deck.length);
     return cards;
   }
 
-  const [hand, setHand] = useState(drawCards(12));
+  useEffect(() => {
+    setHand(drawCards(12));
+  }, []);
 
   function addCards() {
     if (deck.length === 0 || hand.length === 15) {
       return;
     }
-    if (deck.length <= 3) {
-      setHand([...hand, ...drawCards(deck.length)]);
-      return;
-    }
-    setHand([...hand, ...drawCards(3)]);
+    const cardsToDraw = deck.length <= 3 ? deck.length : 3;
+    setHand((prevHand) => [...prevHand, ...drawCards(cardsToDraw)]);
     return;
   }
 
-  function handleCardClick(index: number) {
-    const cardCopy: LiveCardProps = Object.assign(hand[index], {
-      selected: !hand[index].selected,
-    });
-    if (selectedCards.length === 3 && cardCopy.selected) {
-      cardCopy.selected = false;
-      return;
-    }
-    const handCopy = hand.slice();
-    handCopy[index] = cardCopy;
-    setHand(handCopy);
+  function toggleCardSelection(index: number) {
+    setHand(
+      hand.map((card, i) =>
+        i === index ? { ...card, selected: !card.selected } : card
+      )
+    );
   }
-
-  var selectedCards = hand.filter((card) => card.selected);
-
-  // useEffect(() => {
-    if (selectedCards.length === 3) {
-      const foundSet = checkForSet(selectedCards);
-      if (foundSet) {
-        console.log("Found a set!");
-        const handWithSetRemoved = hand.filter((card) => !card.selected)
-        setHand(handWithSetRemoved);
-        addCards();
-        console.log(deck)
-      } else {
-        console.log("Not a set.")
-      }
+  
+  function handleCardClick(index: number) {
+    toggleCardSelection(index);
+  }
+  
+  let selectedCards = hand.filter((card) => card.selected);
+  if (selectedCards.length === 3) {
+    const foundSet = checkForSet(selectedCards);
+    if (foundSet) {
+      console.log("Found a set!");
+      const handWithSetRemoved = hand.filter((card) => !card.selected);
+      setHand(handWithSetRemoved);
+      addCards();
+      console.log(deck);
+    } else {
+      console.log("Not a set.");
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, [selectedCards]);
+  }
 
   function allEqualOrAllDifferent(array: string[]): boolean {
     return new Set(array).size === 1 || new Set(array).size === 3;
@@ -80,8 +77,8 @@ export const SetGame: React.FC = () => {
   return (
     <div className="game">
       <h1>Set Card Game</h1>
-      <SetHand hand={hand} onCardClick={handleCardClick}/>
+      <SetHand hand={hand} onCardClick={handleCardClick} />
       <button onClick={addCards}>Add 3 Cards</button>
     </div>
   );
-}
+};
